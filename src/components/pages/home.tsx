2e4b41ce69d76f8956b3421520100ed28eb02e16
Nathalia Rus <nathaliarus@live.fr>
2019-09-19 18:59:04 +0100

    remove unecessary comments, onto atomic design

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -67,16 +67,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(HomePage);
-
-// const HomePage = () => (
-//   <div>
-//     <h1>Home Page</h1>
-//     <Button> Submit </Button>
-//     <p>
-//       This website allows you to create tasks and keep track of your to-do list
-//       :) :)
-//     </p>
-//   </div>
-// );
-
-// export default HomePage;
