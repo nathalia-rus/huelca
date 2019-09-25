ee8588415456201f812b8637e8bad45d177eba7c
Nathalia Rus <nathaliarus@live.fr>
2019-09-25 10:21:59 +0100

    add epics actions to createEpicMiddleware

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -2,11 +2,12 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import hueldata from "../reducers/hueldataReducer";
 import formData from "../reducers/formDataReducer";
 import thunk from "redux-thunk";
+import { epics } from "../modules/epics/index";
 import { createStore, applyMiddleware, combineReducers } from "redux";
 const { createEpicMiddleware } = require("redux-observable");
 
 export default function configureStore() {
-  const middlewares = [thunk, createEpicMiddleware()];
+  const middlewares = [thunk, createEpicMiddleware(epics)];
   const middleWareEnhancer = applyMiddleware(...middlewares);
 
   const rootReducer = combineReducers({
