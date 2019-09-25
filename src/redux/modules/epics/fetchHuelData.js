da7dc6db3cfc9cfaba7e5a8fd2b13776663022e6
Nathalia Rus <nathaliarus@live.fr>
2019-09-25 11:07:56 +0100

    solve all errors

--- src/redux/modules/epics/fetchHuelData.js
+++ src/redux/modules/epics/fetchHuelData.js
@@ -1,17 +1,20 @@
 import { mapTo } from "rxjs/operators";
 import "rxjs/add/observable/of";
+import { mergeMap } from "rxjs/operators";
+import { ofType } from "redux-observable";
 import { Observable } from "rxjs/Observable";
 import data from "../../hueldata.json";
 import {
   FETCH_HUEL_DATA_EPIC_SUCCESS,
   FETCH_HUEL_DATA_EPIC
 } from "../actions/actionTypes";
-import { fetchHuelDataEpicError } from "../actions/actions";
+import { fetchHuelDataEpicError } from "../actions/actioncreators";
 
-const fetchHuelDataEpic = action$ =>
-  action$.ofType(FETCH_HUEL_DATA_EPIC).mergeMap(() => {
-    const response = data;
-    if (response.statys === 200) {
+export const fetchHuelDataEpic = action$ => {
+  return action$.pipe(
+    ofType(FETCH_HUEL_DATA_EPIC),
+    mergeMap(() => {
+      const response = data;
       mapTo({
         type: FETCH_HUEL_DATA_EPIC_SUCCESS,
         powders: response.powders,
@@ -19,10 +22,6 @@ const fetchHuelDataEpic = action$ =>
       }).catch(err => {
         return Observable.of(fetchHuelDataEpicError(err));
       });
-    } else {
-      console.log("there's been a request error");
-      return Observable.of(fetchHuelDataEpicError("there's been an error"));
-    }
-  });
-
-export default fetchHuelDataEpic;
+    })
+  );
+};
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -7,7 +7,8 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 const { createEpicMiddleware } = require("redux-observable");
 
 export default function configureStore() {
-  const middlewares = [thunk, createEpicMiddleware(epics)];
+  const epicMiddleware = createEpicMiddleware();
+  const middlewares = [thunk, epicMiddleware];
   const middleWareEnhancer = applyMiddleware(...middlewares);
 
   const rootReducer = combineReducers({
@@ -20,5 +21,10 @@ export default function configureStore() {
     composeWithDevTools(middleWareEnhancer)
   );
 
+  epicMiddleware.run(epics);
+
   return store;
 }
+
+// link to explain why this new setup for epics -
+// https://redux-observable.js.org/MIGRATION.html#setting-up-the-middleware
